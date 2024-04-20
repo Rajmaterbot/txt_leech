bedbcc72e73e1ed5599b53214e5328bacf88ede4
Raj Saini <98858458+Rajmaterbot@users.noreply.github.com>
2024-04-20 21:53:12 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "23581132"
+api_hash = "f600844adba85094cfc1a608e2e737c0"
+bot_token = "7013751010:AAE0cOdS4qwgKDRMNXbGQ5tzqtz4L5Z5zTQ"
