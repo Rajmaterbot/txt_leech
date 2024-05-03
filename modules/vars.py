c9811939900ff1f00a7da9693564c15f232029d9
Raj Saini <98858458+Rajmaterbot@users.noreply.github.com>
2024-05-03 21:45:03 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "16712603"
 api_hash = "62e7bdb76c9d12f90021a5a90bb2402f"
-bot_token = "7186325862:AAFUxB2rKj0SjkN6h57keIdPD2cGAnG6pSc"
+bot_token = "6486364255:AAF53k8xl0lL9foWmP5IgXymk2TtrYrwbas"
